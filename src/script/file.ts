0cb451c0112d934400af8ee530bb80261049af54
ibro <sameoldlab@proton.me>
2025-09-20 21:37:37 +0100

    cross-platform mime fallback

--- src/script/file.ts
+++ src/script/file.ts
@@ -2,20 +2,46 @@ const { os } = Deno.build
 const OS_CMD: Record<string, { cmd: string, args: string[] }> = Object.freeze({
   darwin: { cmd: "file", args: ["--mime-type", "-b"] },
   linux: { cmd: "file", args: ["--mime-type", "-b"] },
+  fallback: { cmd: "file", args: ["--mime-type", "-b"] },
 })
 
 /** Get MIME type using file command */
 export function getMimeTypeSync(filePath: string): string {
-  if (!Object.keys(OS_CMD).includes(os)) throw new Error(`unsupported os: ${os}`)
-
-  const output = new Deno.Command(OS_CMD[os].cmd, {
-    args: [...OS_CMD[os].args, filePath],
-    stdout: "piped",
-  }).outputSync();
-  if (output.code === 0) {
-    return new TextDecoder().decode(output.stdout).trim();
+  if (!Object.keys(OS_CMD).includes(os)) {
+    console.error(`unsupported os: ${os}. trying default command`)
   }
 
+  try {
+    const output = new Deno.Command(OS_CMD[os].cmd ?? OS_CMD.fallback.args, {
+      args: [...OS_CMD[os].args ?? OS_CMD.fallback.args, filePath],
+      stdout: "piped",
+    }).outputSync();
+    if (output.code === 0) {
+      return new TextDecoder().decode(output.stdout).trim();
+    }
+  } catch (e) {
+    console.error(e)
+  }
+  return 'text/plain';
+}
+
+/** Get MIME type using file command */
+export async function getMimeType(filePath: string): Promise<string> {
+  if (!Object.keys(OS_CMD).includes(os)) {
+    console.warn(`unsupported os: ${os}. trying default command`)
+  }
+
+  try {
+    const output = await new Deno.Command(OS_CMD[os].cmd ?? OS_CMD.fallback.args, {
+      args: [...OS_CMD[os].args ?? OS_CMD.fallback.args, filePath],
+      stdout: "piped",
+    }).output();
+    if (output.code === 0) {
+      return new TextDecoder().decode(output.stdout).trim();
+    }
+  } catch (e) {
+    console.error(e)
+  }
   return 'text/plain';
 }
 
